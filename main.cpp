9e717b2ecad2dbec9592e241de28685f3a14ae41
KASAHARAITUKI <hj-g20007@sist.ac.jp>
2021-05-07 12:29:16 +0900

    四角、円

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 #define GAME_HEIGHT	720					// ゲーム画面の高さ(ハイト)
 #define GAME_COLOR	32					// ゲームの色域
 
-#define GAME_ICON_ID	333					// ゲームのICONのID
+#define GAME_ICON_ID	333				// ゲームのICONのID
+
+#define GAME_WINDOW_BAR 0				// ウィンドウバーの種類	
 
 // プログラムは WinMain から始まります
 // windowsのプログラミング方法 = (WinAPI)で動いている！
@@ -25,6 +27,10 @@ int WINAPI WinMain(
 	SetWindowSize(GAME_WIDTH, GAME_HEIGHT);				// ウィンドウの大きさを設定
 	SetBackgroundColor(255, 255, 255);					// デフォルトの背景の色
 	SetWindowIconID(GAME_ICON_ID);						// アイコンファイルを読み込み
+	SetWindowStyleMode(GAME_WINDOW_BAR);				// ウィンドウバーの状態
+	SetWaitVSyncFlag(TRUE);								// ディスプレイの垂直同期を有効にする
+	SetAlwaysRunFlag(TRUE);								// ウィンドウをずっとアクティブにする
+
 
 	// ＤＸライブラリ初期化処理
 	if (DxLib_Init() == -1)
@@ -32,6 +38,24 @@ int WINAPI WinMain(
 		// エラーが起きたら直ちに終了
 		return -1;
 	}
+
+	// 四角の位置を決める
+	int x = GAME_WIDTH / 2;
+	int y = GAME_HEIGHT / 2;
+
+	// 速度
+	int xspeed = 10;
+	int yspeed = 4;
+
+	// 四角の大きさを決める
+	int width = 32;		// 幅
+	int height = 32;	// 高さ
+
+	// 円の半径を決める
+	int radius = 100;
+
+	// ダブルバッファリング有効化
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	// 無限ループ
 	while (1)
@@ -49,6 +73,47 @@ int WINAPI WinMain(
 			// 無限ループをぬける
 			break;
 		}
+
+		// 画面を消去する
+		if (ClearDrawScreen() != 0) { break; }
+
+		// 四角を描写
+		DrawBox(
+			x, y, width, height,
+			GetColor(255, 0, 0),	// 色を取得
+			true					// 塗りつぶし？
+		);
+
+		// 円を描画
+		//DrawCircle(
+		//	x, y, radius,
+		//	GetColor(0, 255, 0),
+		//	FALSE, 5
+		//);
+
+		x += xspeed;				// 四角の位置を右にずらす
+		y += yspeed;	
+
+		if (x < 0 || x + width > GAME_WIDTH)
+		{
+			xspeed = -xspeed;
+		}
+		if (y < 0 || y + height > GAME_HEIGHT)
+		{
+			yspeed = -yspeed;
+		}
+
+		//if (x - radius < 0 || x + radius > GAME_WIDTH)
+		//{
+		//	xspeed = -xspeed;
+		//}
+		//if (y - radius < 0 || y + radius > GAME_HEIGHT)
+		//{
+		//	yspeed = -yspeed;
+		//}
+
+		ScreenFlip();				// ダブルバッファリング下画面を描画
+		
 	}
 
 	// ＤＸライブラリ使用の終了処理
