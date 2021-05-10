e27f6382198abbb6f8ff792abff30f2b4b07d0a3
KASAHARAITUKI <hj-g20007@sist.ac.jp>
2021-05-10 12:30:43 +0900

    keyboard

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ int WINAPI WinMain(
 	LPSTR lpCmdLine, 
 	int nCmdShow)
 {
+	SetOutApplicationLogValidFlag(FALSE);
+	// Log.txtを出力しない
 
 	ChangeWindowMode(TRUE);								// ウィンドウモードに設定
 	SetMainWindowText(GAME_TITLE);						// ウィンドウのタイトルの文字
@@ -39,20 +41,6 @@ int WINAPI WinMain(
 		return -1;
 	}
 
-	// 四角の位置を決める
-	int x = GAME_WIDTH / 2;
-	int y = GAME_HEIGHT / 2;
-
-	// 速度
-	int xspeed = 10;
-	int yspeed = 4;
-
-	// 四角の大きさを決める
-	int width = 32;		// 幅
-	int height = 32;	// 高さ
-
-	// 円の半径を決める
-	int radius = 100;
 
 	// ダブルバッファリング有効化
 	SetDrawScreen(DX_SCREEN_BACK);
@@ -60,57 +48,8 @@ int WINAPI WinMain(
 	// 無限ループ
 	while (1)
 	{
-		// 何かしらキーがおされたとき
-		if (CheckHitKeyAll() != 0)
-		{
-			// 無限ループをぬける
-			break;
-		}
-
-		// メッセージを受け取り続ける
-		if (ProcessMessage() != 0)	// -1のとき、エラーやウィンドウが閉じられた
-		{
-			// 無限ループをぬける
-			break;
-		}
-
-		// 画面を消去する
-		if (ClearDrawScreen() != 0) { break; }
-
-		// 四角を描写
-		DrawBox(
-			x, y, width, height,
-			GetColor(255, 0, 0),	// 色を取得
-			true					// 塗りつぶし？
-		);
-
-		// 円を描画
-		//DrawCircle(
-		//	x, y, radius,
-		//	GetColor(0, 255, 0),
-		//	FALSE, 5
-		//);
-
-		x += xspeed;				// 四角の位置を右にずらす
-		y += yspeed;	
-
-		if (x < 0 || x + width > GAME_WIDTH)
-		{
-			xspeed = -xspeed;
-		}
-		if (y < 0 || y + height > GAME_HEIGHT)
-		{
-			yspeed = -yspeed;
-		}
-
-		//if (x - radius < 0 || x + radius > GAME_WIDTH)
-		//{
-		//	xspeed = -xspeed;
-		//}
-		//if (y - radius < 0 || y + radius > GAME_HEIGHT)
-		//{
-		//	yspeed = -yspeed;
-		//}
+		if (ProcessMessage() != 0) { break; }	// メッセージを受け取り続ける	
+		if (ClearDrawScreen() != 0) { break; }	// 画面を消去する
 
 		ScreenFlip();				// ダブルバッファリング下画面を描画
 		
@@ -119,6 +58,5 @@ int WINAPI WinMain(
 	// ＤＸライブラリ使用の終了処理
 	DxLib_End();
 
-	// ソフトの終了
-	return 0; 
+	return 0; 	// ソフトの終了
 }
